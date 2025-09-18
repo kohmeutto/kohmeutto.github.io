bc6b51ac73d084f22054d39c50927f7a82fd1251
kohmeutto <kohmeutto@gmail.com>
2025-09-18 09:56:33 +0900

    .

--- searchindex.en.js
+++ searchindex.en.js
@@ -489,8 +489,8 @@ var relearn_searchindex = [
   },
   {
     "breadcrumb": "수리전자물리학",
-    "content": "(a) Hibert space\r(a) Ket",
-    "description": "(a) Hibert space\r(a) Ket",
+    "content": "(a) Hibert space\r(a) Ket\r(a) Bra",
+    "description": "(a) Hibert space\r(a) Ket\r(a) Bra",
     "tags": [],
     "title": "Dirac notation",
     "uri": "/dirac_notation/index.html"
@@ -558,6 +558,14 @@ var relearn_searchindex = [
     "tags": [],
     "title": "(a) Basis \u0026 Components",
     "uri": "/temp/a_basis_components/index.html"
+  },
+  {
+    "breadcrumb": "수리전자물리학 \u003e Dirac notation",
+    "content": "1. 유한차원의 내적 (1) 유클리드 공간\n“$\\vec{a}\\cdot$” 은 연산자이다. 피 연산자를 요구한다. 이것은 행벡터로 표현된다. 따라서, 행벡터를 의미에 따라, 연산자로 간주할 수 있다.\n$$\r\\vec{a}\\cdot:=[a]^T\r$$$$\r\\vec{a}\\cdot\\vec{b}=[a]^T[b]\r$$(2) 힐버트 공간\n$$\r\\langle a| \\text{ 는 } \\vec{a}\\cdot \\text{에 대응한다.}\r$$$$\r\\langle a|b\\rangle \\text{ 는 } \\vec{a}\\cdot\\vec{b} \\text{ 에 대응한다.}\r$$ 2. 유한차원의 항등 연산자 $$\r\\hat{I} = \\sum_i |u_i\\rangle \\langle u_i|\r$$proof)\n$$\r|\\psi\\rangle = \\sum_i u_i|u_i\\rangle\r$$여기에서, $|u_i\\rangle$ 는 단위 기저 벡터이다. 단위 기저 벡터와 내적을 하면,\n$$\ru_i=\\langle u_i|\\psi\\rangle\r$$$$\r|\\psi\\rangle = \\sum_i \\langle u_i|\\psi\\rangle|u_i\\rangle\r= \\sum_i |u_i\\rangle \\langle u_i|\\psi\\rangle\r$$따라서,\n$$\r\\hat{I} = \\sum_i |u_i\\rangle \\langle u_i|\r$$ 3. 무한차원의 항등 연산자 $$\r\\hat{I} = \\int dx |x\\rangle \\langle x|\r$$proof)\n$$\r|\\psi\\rangle = \\int dx \\langle x|\\psi\\rangle |x\\rangle = \\int dx |x\\rangle \\langle x|\\psi\\rangle $$따라서,\n$$\r\\hat{I} = \\int dx |x\\rangle \\langle x|\r$$ 4. 무한차원의 내적 힐버트 공간에서, 함수 f(x)와 함수 g(x)의 내적을 표현할 때,\n$$\r\\langle f|g\\rangle = \\int dx f^{*}(x) g(x)\r$$proof)\n$$\r\\langle f|g\\rangle = \\langle f|\\hat{I}|g\\rangle\r= \\langle f| \\left( \\int dx |x\\rangle \\langle x| \\right) |g\\rangle\r= \\int dx f^{*}(x) g(x)\r$$",
+    "description": "1. 유한차원의 내적 (1) 유클리드 공간\n“$\\vec{a}\\cdot$” 은 연산자이다. 피 연산자를 요구한다. 이것은 행벡터로 표현된다. 따라서, 행벡터를 의미에 따라, 연산자로 간주할 수 있다.\n$$\r\\vec{a}\\cdot:=[a]^T\r$$$$\r\\vec{a}\\cdot\\vec{b}=[a]^T[b]\r$$(2) 힐버트 공간\n$$\r\\langle a| \\text{ 는 } \\vec{a}\\cdot \\text{에 대응한다.}\r$$$$\r\\langle a|b\\rangle \\text{ 는 } \\vec{a}\\cdot\\vec{b} \\text{ 에 대응한다.}\r$$ 2. 유한차원의 항등 연산자 $$\r\\hat{I} = \\sum_i |u_i\\rangle \\langle u_i|\r$$proof)\n$$\r|\\psi\\rangle = \\sum_i u_i|u_i\\rangle\r$$여기에서, $|u_i\\rangle$ 는 단위 기저 벡터이다. 단위 기저 벡터와 내적을 하면,",
+    "tags": [],
+    "title": "(a) Bra",
+    "uri": "/dirac_notation/a_bra/index.html"
   },
   {
     "breadcrumb": "수리전자물리학 \u003e Tensor analysis \u003e Calculus",
